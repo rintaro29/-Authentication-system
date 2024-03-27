669120bb37bee70f83f226acbd323857fe77876a
rintaro1029 <tomitaka1029@gmail.com>
2024-03-27 18:46:02 +0900

    setting nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/navigation/Navigation";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,17 +11,14 @@ export const metadata: Metadata = {
   description: "Prisma Auth",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
   return (
     <html>
       <body className={inter.className}>
         <div>
           <div className="flex min-h-screen flex-col">
-            <Navigation />
+            <Navigation currentUser={currentUser} />
             <main className="container mx-auto max-w-screen-sm flex-1 px-1 py-5">{children}</main>
 
             <footer className="py-5">
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import getCurrentUser from "./actions/getCurrentUser";
 
-export default function Home() {
-  return <div>Hello</div>;
-}
+// メインページ
+const Home = async () => {
+  const currentUser = await getCurrentUser();
+
+  return <div className="text-center">{currentUser ? <div>認証中</div> : <div>未認証</div>}</div>;
+};
+
+export default Home;
