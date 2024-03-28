6079e43a5f7128ff1ace795d2da3350c7a1e0941
rintaro1029 <tomitaka1029@gmail.com>
2024-03-28 19:16:56 +0900

    setting signup modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Navigation from "./components/navigation/Navigation";
-import getCurrentUser from "./actions/getCurrentUser";
-
+import { Inter } from "next/font/google";
+import Navigation from "@/app/components/navigation/Navigation";
+import AuthContext from "@/app/context/AuthContext";
+import ToasterContext from "@/app/context/ToasterContext";
+import SignupModal from "@/app/components/modals/SignupModal";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Prisma Auth",
-  description: "Prisma Auth",
+export const metadata = {
+  title: "Prisma Blog",
+  description: "Prisma Blog",
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const currentUser = await getCurrentUser();
+
   return (
     <html>
       <body className={inter.className}>
-        <div>
+        <AuthContext>
+          {/* Toaster */}
+          <ToasterContext />
+          {/* モーダル */}
+          <SignupModal />
           <div className="flex min-h-screen flex-col">
             <Navigation currentUser={currentUser} />
+
             <main className="container mx-auto max-w-screen-sm flex-1 px-1 py-5">{children}</main>
 
             <footer className="py-5">
-              <div className="text-center text-sm">This is Footer</div>
+              <div className="text-center text-sm">Copyright © All rights reserved | FullStackChannel</div>
             </footer>
           </div>
-        </div>
+        </AuthContext>
       </body>
     </html>
   );
